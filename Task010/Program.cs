f42524893a1212e3145eaa74aa119e9ded483764
Tori <mes.viktoria@mail.ru>
2022-08-25 23:14:10 +0500

    2 цифра трёхзначного числа

---
+++ Task010/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 10
+
+Console.WriteLine("Пожалуйста, введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+if(99 < number && number < 1000) 
+{
+   int a2 = number / 10 % 10;
+   Console.WriteLine($"Число: {number}, вторая цифра числа: {a2}"); 
+}
+else
+{
+    Console.WriteLine($"Число {number} не трёхзначное");
+}
+
