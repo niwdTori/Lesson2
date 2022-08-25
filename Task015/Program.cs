e38b5512deb82a1915197a3f7bea7c8c04073f64
Tori <mes.viktoria@mail.ru>
2022-08-25 23:24:13 +0500

    Выходной или нет

---
+++ Task015/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 15
+
+Console.WriteLine("Пожалуйста, введите цифру, обозначающую день недели: ");
+int number = int.Parse(Console.ReadLine());
+
+if(number > 5 && number < 8)
+{
+    Console.WriteLine("Выходной!");
+}
+else if(number > 7) 
+{
+    Console.WriteLine("В неделе только 7 дней!");
+}
+else
+{
+    Console.WriteLine("Не выходной :(");
+}
