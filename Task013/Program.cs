86ccb8cee6f115f5e94623ebbe917aa309d55800
Tori <mes.viktoria@mail.ru>
2022-08-26 00:11:11 +0500

    Третья цифра заданного числа

---
+++ Task013/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13
+
+Console.WriteLine("Пожалуйста, введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if(number > 99) 
+{
+    while(number > 1000)
+    {
+        number = number / 10;
+    }
+    int a3 = number % 10;
+    Console.WriteLine($"Третья цифра: {a3}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
